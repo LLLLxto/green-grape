45c3c27de8881b36694eefe999ed7457a8946fa3
Tong <LLLLxto@163.com>
2021-08-16 15:46:26 +0800

    Charge 页面布局

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="nav">
     <router-link to="/charge">记账</router-link>
     |
     <router-link to="bill">明细</router-link>
@@ -15,5 +15,7 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+.nav{
+  border:1px solid red;
+}
 </style>
--- src/views/Charge.vue
+++ src/views/Charge.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    Charge.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      Charge.vue
+    </div>
+
     <Nav/>
   </div>
 </template>
@@ -12,5 +15,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+.nav-wrapper{
+  border:1px solid green;
+  display:flex;
+  flex-direction:column;
+  height: 100vh;
+}
+.content{
+  border:1px solid blue;
+  overflow:auto; //如果 div.content 内容溢出可滚动
+  flex-grow:1; //尽量把所有高度都给 div.content
+}
 </style>
