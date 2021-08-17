632f4281402785aa164c1cd4c3f30a9a7efdc1e8
Tong <LLLLxto@163.com>
2021-08-17 13:24:04 +0800

    添加 Nav 样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="nav">
-    <router-link to="/charge">
-      <Icon name="charge"/>
-      记账
-    </router-link>
-    |
-    <router-link to="bill">
+  <nav>
+    <router-link to="bill" class="item">
       <Icon name="bill"/>
       明细
     </router-link>
-    |
-    <router-link to="charts">
+
+    <router-link to="/charge" class="item">
+      <Icon name="charge"/>
+      记账
+    </router-link>
+
+    <router-link to="charts" class="item">
       <Icon name="charts"/>
       图表
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -24,7 +24,23 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.nav{
-  border:1px solid red;
+nav{
+  display: flex;
+  flex-direction: row;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  font-size: 12px;
+  > .item{
+    padding: 2px 0;
+    width: 33.33333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    .icon {
+      width: 32px;
+      height: 32px;
+    }
+  }
 }
+
 </style>
