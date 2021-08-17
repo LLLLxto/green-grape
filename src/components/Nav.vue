272cbe5a9232875e0fa4cedfa3d42ea981b2b527
Tong <LLLLxto@163.com>
2021-08-17 10:09:51 +0800

    创建 Icon 组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,27 +1,23 @@
 <template>
   <div class="nav">
     <router-link to="/charge">
-      <svg>
-        <use xlink:href="#bill"/>
-      </svg>
+      <Icon name="charge"/>
       记账
     </router-link>
     |
-    <router-link to="bill">明细</router-link>
+    <router-link to="bill">
+      <Icon name="bill"/>
+      明细
+    </router-link>
     |
-    <router-link to="charts">图表</router-link>
+    <router-link to="charts">
+      <Icon name="charts"/>
+      图表
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
-let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext)
-try{
-  importAll(require.context('../assets/icons', true, /\.svg$/))
-}catch(error){
-  console.log(error)
-}
-
-
 export default {
   name: 'Nav'
 };
