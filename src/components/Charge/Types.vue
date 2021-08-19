fa6efcdd5c4a9d3e417a07ceacb868bfcd35eddc
Tong <LLLLxto@163.com>
2021-08-19 23:02:06 +0800

    TS 实现组件

--- src/components/Charge/Types.vue
+++ src/components/Charge/Types.vue
@@ -7,26 +7,42 @@
   </ul>
 </template>
 
-<script>
-export default {
-  name: 'Types',
-  props:['xxx'],
-  mounted(){
-    console.log(this.xxx)
-  },
-  data(){
-    return{
-      type:'-' //'-'表示支出，'+'表示收入
+<script lang="ts">
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator'; //引入装饰器
+@Component //装饰器
+export default class Types extends Vue {
+  type = '-'; // '-'表示支出，'+'表示收入 //data
+  selectType(type: string) { //type 只能是'-'/'+'中的一个 //methods
+    if (type !== '-' && type !== '+') {
+      throw new Error('type is unknown');
     }
-  },
-  methods:{
-    selectType(type){ //type 只能是'-'/'+'中的一个
-      if(type !== '-' && type !== '+') throw new Error('type is unknown')
-      this.type=type
-    }
+    this.type = type;
   }
-};
+}
 </script>
+
+
+<!--<script>-->
+<!--export default {-->
+<!--  name: 'Types',-->
+<!--  props:['xxx'],-->
+<!--  mounted(){-->
+<!--    console.log(this.xxx)-->
+<!--  },-->
+<!--  data(){-->
+<!--    return{-->
+<!--      type:'-' //'-'表示支出，'+'表示收入-->
+<!--    }-->
+<!--  },-->
+<!--  methods:{-->
+<!--    selectType(type){ //type 只能是'-'/'+'中的一个-->
+<!--      if(type !== '-' && type !== '+') throw new Error('type is unknown')-->
+<!--      this.type=type-->
+<!--    }-->
+<!--  }-->
+<!--};-->
+<!--</script>-->
 
 <style scoped lang="scss">
 @import "~@/assets/style/helper.scss";
