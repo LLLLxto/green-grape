3e5c46dbc4cc5af2b9070c5852e2a1256e4dee72
Tong <LLLLxto@163.com>
2021-08-29 17:46:50 +0800

    删除 models，使用模块化的 store

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,23 +1,9 @@
-import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
-import router from '@/router';
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
 
 const store = {
-  recordList: recordListModel.fetch(),
-  createRecord: (record: RecordItem) => recordListModel.create(record),
-  tagList: tagListModel.fetch(),
-  createTag: (name: string) => {
-    const message = tagListModel.create(name);
-    if (message === 'duplicated') {
-      window.alert('分类名称已经存在啦');
-    } else if (message === 'success') {
-      window.alert('保存成功');
-      router.back();
-    }
-  },
-  removeTag: (id: string) => {
-    return tagListModel.remove(id);
-  }
+  ...recordStore,
+  ...tagStore
 };
 
 export default store;
---
+++ src/store/tagStore.ts
@@ -0,0 +1,43 @@
+import router from '@/router';
+import createId from '@/lib/createId';
+
+const localStorageKeyName = 'tagList';
+const tagStore = {
+  tagList: [] as Tag[],
+  fetchTags() {
+    this.tagList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]');
+    return this.tagList;
+  },
+  saveTags() {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.tagList));
+  },
+  createTag(name: string) {
+    const names = this.tagList.map(item => item.name);
+    if (names.indexOf(name) >= 0) {
+      window.alert('分类名称已经存在啦');
+      return 'duplicated';
+    }
+    const id = createId().toString();
+    this.tagList.push({id: id, name: name});
+    this.saveTags();
+    window.alert('保存成功');
+    router.back();
+    return 'success';
+  },
+  removeTag(id: string) {
+    let index = -1;
+    for (let i = 0; i < this.tagList.length; i++) {
+      if (this.tagList[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+    this.tagList.splice(index, 1);
+    this.saveTags();
+    return true;
+  }
+};
+
+tagStore.fetchTags()
+
+export default tagStore;
